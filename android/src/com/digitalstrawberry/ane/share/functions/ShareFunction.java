4b1a3cdc0f7b19df9329722d771e8c4a8be5bc77
Marcel Piešťanský <marcel@piestansky.cz>
2020-10-20 15:33:43 +0200

    Support AndroidX libs (ref #14)

--- android/src/com/digitalstrawberry/ane/share/functions/ShareFunction.java
+++ android/src/com/digitalstrawberry/ane/share/functions/ShareFunction.java
@@ -29,7 +29,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 import com.adobe.air.AndroidActivityWrapper;
 import com.adobe.air.IANEShareActivityResultCallback;
 import com.adobe.fre.*;
